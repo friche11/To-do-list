8c5582a58853610cedff0b3cad14bfed19bb13a8
friche11 <vicfriche@gmail.com>
2024-03-26 03:42:36 -0300

    Task controller

--- src/main/java/com/labdessoft/roteiro01/controller/TaskController.java
+++ src/main/java/com/labdessoft/roteiro01/controller/TaskController.java
@@ -1,12 +1,52 @@
 package com.labdessoft.roteiro01.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.labdessoft.roteiro01.entity.Task;
+import com.labdessoft.roteiro01.service.TaskService;
+
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
-public class TaskController {
-    @GetMapping("/task")
-    public String listAll(){
-        return "Listar todas as tasks";
+public class TaskController extends Exception{
+
+    @Autowired
+     private TaskService taskService;
+
+    @Operation(summary = "Lista todas as tarefas da lista")
+    @GetMapping("/tasks")
+    public List<Task> listAll(){
+        return taskService.listAll();
     }
+
+    @GetMapping("/task/{id}")
+    public Optional<Task> getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+    
+    @PostMapping("/create/task")
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @PutMapping("/edit/{id}")
+    public Task editTask(@RequestBody Task task) {
+        return taskService.editTask(task);
+    }
+    
+    @DeleteMapping("/delete/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
+
 }
--- src/main/java/com/labdessoft/roteiro01/service/TaskService.java
+++ src/main/java/com/labdessoft/roteiro01/service/TaskService.java
@@ -4,16 +4,18 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.labdessoft.roteiro01.entity.Task;
 import com.labdessoft.roteiro01.repository.TaskRepository;
 
+@Service
 public class TaskService {
     
 @Autowired
     private TaskRepository taskRepository;
     
-    public List<Task> getAllTasks() {
+    public List<Task> listAll() {
         return taskRepository.findAll();
     }
     
@@ -24,7 +26,11 @@ public class TaskService {
     public Task createTask(Task task) {
         return taskRepository.save(task);
     }
-    
+
+    public Task editTask(Task task) {
+        return taskRepository.save(task);
+    }
+
     public void deleteTask(Long id) {
         taskRepository.deleteById(id);
     }
