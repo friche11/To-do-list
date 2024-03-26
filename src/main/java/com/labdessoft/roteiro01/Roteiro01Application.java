a3413e467f303ead36ef24234e1623cdcb45f4a9
friche11 <vicfriche@gmail.com>
2024-03-26 00:40:51 -0300

    Iniciando projeto to-do list de laboratorio

---
+++ src/main/java/com/labdessoft/roteiro01/Roteiro01Application.java
@@ -0,0 +1,13 @@
+package com.labdessoft.roteiro01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Roteiro01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Roteiro01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/labdessoft/roteiro01/Roteiro01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labdessoft.roteiro01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Roteiro01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
