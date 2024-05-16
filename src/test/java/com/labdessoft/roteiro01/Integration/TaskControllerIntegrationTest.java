57119f19e4f01b73e9788e86d30f40164962351c
friche11 <vicfriche@gmail.com>
2024-05-16 12:03:56 -0300

    test: testes de integração com a API

--- src/test/java/com/labdessoft/roteiro01/Integration/TaskControllerIntegrationTest.java
+++ src/test/java/com/labdessoft/roteiro01/Integration/TaskControllerIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.labdessoft.roteiro01.Integration;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,9 +13,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import static io.restassured.RestAssured.get;
 import static org.hamcrest.Matchers.equalTo;
+import com.labdessoft.roteiro01.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 
 
 import com.labdessoft.roteiro01.Roteiro01Application;
+import com.labdessoft.roteiro01.entity.Task;
+import com.labdessoft.roteiro01.enums.TaskPriority;
+import com.labdessoft.roteiro01.enums.TaskType;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(JUnitPlatform.class)
@@ -21,22 +34,97 @@ import com.labdessoft.roteiro01.Roteiro01Application;
 = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
 public class TaskControllerIntegrationTest {
-    
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    private int taskId; 
+
 @Before
 public void setup() {
 RestAssured.baseURI = "http://localhost:8080";
 RestAssured.port = 8080;
+
 }
-@Test
-public void
-givenUrl_whenSuccessOnGetsResponseAndJsonHasRequiredKV_thenCorrect() {
-get("/api/tasks").then().statusCode(200);
-}
-@Test
-public void
-givenUrl_whenSuccessOnGetsResponseAndJsonHasOneTask_thenCorrect() {
-get("/api/tasks/1").then().statusCode(200)
-.assertThat().body("description",
-equalTo("Primeira tarefa"));
-}
+
+    @Test
+    public void
+    givenUrl_whenSuccessOnGetsResponseAndJsonHasRequiredKV_thenCorrect() {
+    get("/api/tasks").then().statusCode(200);
+    }
+
+    @Test
+    public void
+    givenUrl_whenSuccessOnGetsResponseAndJsonHasOneTask_thenCorrect() {
+    get("/api/task/1").then().statusCode(200)
+    .assertThat().body("description",
+    equalTo("Primeira tarefa"));
+    }
+
+    @Test
+    public void givenUrl_whenSuccessOnPostCreatesTask_thenCorrect() {
+        // Criar a tarefa e salvar no banco de dados
+        Task task = new Task();
+        task.setDescription("Tarefa para testes");
+        task.setCompleted(false);
+        task.setDueDate(LocalDate.of(2024, 5, 10));
+        task.setDueDays(5);
+        task.setType(TaskType.PRAZO);
+        task.setPriority(TaskPriority.MEDIA);
+
+        taskId = task.getId();
+    
+        given()
+            .contentType(ContentType.JSON)
+            .body(task)
+        .when()
+            .post("/api/create/task")
+        .then()
+            .statusCode(201);
+    }
+    
+
+    @Test
+    public void givenUrl_whenSuccessOnPutCompletesTask_thenCorrect() {
+        when().put("/api/complete/1")
+            .then().statusCode(200);
+    }
+
+    @Test
+    public void givenUrl_whenSuccessOnPutUpdatesTask_thenCorrect() {
+        Optional<Task> optionalTask = taskRepository.findById(taskId);
+        if (optionalTask.isPresent()) {
+            Task updatedTask = optionalTask.get();
+            updatedTask.setDescription("Tarefa atualizada");
+            updatedTask.setId(taskId);
+    
+            given()
+                .contentType(ContentType.JSON)
+                .body(updatedTask)
+            .when()
+                .put("/api/edit/" + taskId)
+            .then()
+                .statusCode(200);
+        }
+    }
+    
+
+    @Test
+    public void givenUrl_whenFailureOnGetTaskById_thenNotFound() {
+        when().get("/api/task/9999")
+            .then().statusCode(404);
+    }
+
+    @Test
+    public void givenUrl_whenSuccessOnDeleteRemovesTask_thenCorrect() {
+        Optional<Task> optionalTask = taskRepository.findById(taskId);
+        if (optionalTask.isPresent()) {
+        when()
+            .delete("/api/delete/" + taskId)
+        .then()
+            .statusCode(204);
+        }
+    }
+
+    
 }
