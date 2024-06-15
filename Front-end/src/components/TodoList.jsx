f83a0a9ae27b995cee3c48a749416d656d414642
friche11 <vicfriche@gmail.com>
2024-06-15 19:51:15 -0300

    feat: icone de excluir tarefa

--- Front-end/src/components/TodoList.jsx
+++ Front-end/src/components/TodoList.jsx
@@ -1,11 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
-
 export const TodoList = ({ task }) => {
   return (
     <div className="Todo">
       <p className={`${task.completed ? "completed" : "incompleted"}`}>
         {task.description}
       </p>
+      <div>
+        <FontAwesomeIcon className="delete-icon" icon={faTrash} />
+      </div>
     </div>
   );
 };
