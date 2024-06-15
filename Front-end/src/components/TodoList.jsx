05ff476cef2ab742a3e312098a162ee2596b7fe8
friche11 <vicfriche@gmail.com>
2024-06-15 20:30:54 -0300

    feat: edição e exclusão da tarefa no TodoWrapper

--- Front-end/src/components/TodoList.jsx
+++ Front-end/src/components/TodoList.jsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+
 export const TodoList = ({ task }) => {
   return (
     <div className="Todo">
@@ -8,6 +9,7 @@ export const TodoList = ({ task }) => {
         {task.description}
       </p>
       <div>
+        <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} />
         <FontAwesomeIcon className="delete-icon" icon={faTrash} />
       </div>
     </div>
--- Front-end/src/components/TodoWrapper.jsx
+++ Front-end/src/components/TodoWrapper.jsx
@@ -2,22 +2,30 @@ import React, { useState } from "react";
 import { TodoForm } from "./TodoForm";
 import { TodoList } from "./TodoList";
 import { v4 as uuidv4 } from "uuid";
-
 export const TodoWrapper = () => {
-  const [todos, setTodos] = useState([
-    { id: 1, description: "Tarefa exemplo", completed: false },
-  ]);
-
-  const addTodo = (description) => {
-    setTodos([...todos, { id: uuidv4(), description: description, completed: false }]);
+  const [todos, setTodos] = useState([]);
+  const addTodo = (todo) => {
+    setTodos([...todos, { id: uuidv4(), description: todo, completed: false }]);
   };
-
+  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
   return (
     <div className="TodoWrapper">
       <h1>Lista de Tarefas</h1>
       <TodoForm addTodo={addTodo} />
-      {todos.map((item) => (
-        <TodoList key={item.id} task={item} />
+      {todos.map((todo) => (
+        <TodoList
+          key={todo.id}
+          task={todo}
+          deleteTodo={deleteTodo}
+          editTodo={editTodo}
+        />
       ))}
     </div>
   );
