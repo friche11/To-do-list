12fa7539dc7b6b6ed1a7383954ec6e5e405a36dc
friche11 <vicfriche@gmail.com>
2024-06-21 16:30:26 -0300

    refactor: front-end utilizando os endpoints

--- Front-end/src/components/TodoList.jsx
+++ Front-end/src/components/TodoList.jsx
@@ -2,15 +2,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
-export const TodoList = ({ task }) => {
+export const TodoList = ({ task, deleteTodo, editTodo }) => {
   return (
     <div className="Todo">
       <p className={`${task.completed ? "completed" : "incompleted"}`}>
         {task.description}
       </p>
       <div>
-        <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} />
-        <FontAwesomeIcon className="delete-icon" icon={faTrash} />
+        <FontAwesomeIcon
+          className="edit-icon"
+          icon={faPenToSquare}
+          onClick={() => editTodo(task.id, { ...task, completed: !task.completed })}
+        />
+        <FontAwesomeIcon
+          className="delete-icon"
+          icon={faTrash}
+          onClick={() => deleteTodo(task.id)}
+        />
       </div>
     </div>
   );
--- Front-end/src/components/TodoWrapper.jsx
+++ Front-end/src/components/TodoWrapper.jsx
@@ -1,20 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TodoForm } from "./TodoForm";
 import { TodoList } from "./TodoList";
-import { v4 as uuidv4 } from "uuid";
+import { getAllTasks, createTask, deleteTask, updateTask } from "../services/taskservice";
+
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
-  const addTodo = (todo) => {
-    setTodos([...todos, { id: uuidv4(), description: todo, completed: false }]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const tasks = await getAllTasks();
+        setTodos(tasks);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+      }
+    };
+
+    fetchTasks();
+  }, []);
+
+  const addTodo = async (description) => {
+    try {
+      const newTask = {
+        description,
+        completed: false,
+        // Set default values for other fields as necessary
+        dueDate: null,
+        dueDays: 0,
+        priority: "Low",
+        status: "Pending",
+        type: "General"
+      };
+      const createdTask = await createTask(newTask);
+      setTodos([...todos, createdTask]);
+    } catch (error) {
+      console.error("Error creating task:", error);
+    }
   };
-  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
-  const editTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
-      )
-    );
+
+  const deleteTodo = async (id) => {
+    try {
+      await deleteTask(id);
+      setTodos(todos.filter((todo) => todo.id !== id));
+    } catch (error) {
+      console.error("Error deleting task:", error);
+    }
   };
+
+  const editTodo = async (id, updatedTask) => {
+    try {
+      const editedTask = await updateTask(id, updatedTask);
+      setTodos(todos.map((todo) => (todo.id === id ? editedTask : todo)));
+    } catch (error) {
+      console.error("Error updating task:", error);
+    }
+  };
+
   return (
     <div className="TodoWrapper">
       <h1>Lista de Tarefas</h1>
